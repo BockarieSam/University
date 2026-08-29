d727784dde4865956df4cc71759fd251a4e867a4
BockarieSam <sambockarie5@gmail.com>
2026-08-29 15:40:50 +0000

    'fix'

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/University",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
